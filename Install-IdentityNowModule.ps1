a2e4e580952b0d99afea01036f2ea931ea86662a
Darren Robinson <darren@darrenjrobinson.com>
2019-09-13 07:27:13 +1000

    Update Install-IdentityNowModule.ps1

--- Install-IdentityNowModule.ps1
+++ Install-IdentityNowModule.ps1
@@ -27,7 +27,7 @@ Copy-Item -Path $sourceFiles -Destination $InstallPath -Recurse
 Pop-Location
 
 # Get-Hash required for IdentityNow Authorization Headers
-if (-not (get-module Pscx)) {Install-Module Pscx -RequiredVersion 3.3.2 -Force -AllowClobber}
+if (-not(Get-Module -ListAvailable -Name pscx)) {Install-Module -Name Pscx -RequiredVersion 3.3.2 -Force -AllowClobber -scope AllUsers}
 
 Import-Module -Name SailPointIdentityNow 
 Get-Command -Module SailPointIdentityNow | Sort-Object Name | Get-Help | Format-Table Name, Synopsis -AutoSize

e3d166b15b47f8610279d0c732d4c06833d2c426
Sean McGovern <smcgover@allegisgroup.com>
2019-11-27 13:13:37 -0500

    fixed Test-IdentityNowCredentials

--- scripts/Test-IdentityNowCredentials.ps1
+++ scripts/Test-IdentityNowCredentials.ps1
@@ -17,10 +17,22 @@ written by Sean McGovern 11/27/2019 (twitter @410sean)
 
 #>
     [cmdletbinding()]
-        
-    $lowusersource=(Get-IdentityNowSource | where {$_.usercount -ne 0} | sort usercount)[0]
-    if ($lowusersource -eq $null){write-warning "testing APIv3 credentials failed, unable to continue";return $null}
-    $accounts=Get-IdentityNowSourceAccounts -sourceID $lowusersource
-    if ($accounts -eq $null){write-warning "able to use APIv3 but testing APIv2 credentials failed";return $null}
+    param ( )
+    try{    
+        $lowusersource=(Get-IdentityNowSource | Where-Object {$_.usercount -ne 0} | Sort-Object usercount)[0]
+    }
+    catch{
+        write-warning "testing APIv3 credentials failed for $($IdentityNowConfiguration.orgName), unable to continue"
+        return $null
+    }
+    "able to use APIv3"
+    try{
+        $accounts=Get-IdentityNowSourceAccounts -sourceID $lowusersource.id
+    }
+    catch{
+        write-warning "testing APIv2 credentials failed for $($IdentityNowConfiguration.orgName)"
+        return $null
+    }
+    "able to use APIv2"
     return "credentials APIv2 and APIv3 are working for $($IdentityNowConfiguration.orgName)"
 }

96626870e6f91c159e0563755e399229a6d70d1e
Sean McGovern <410sean@gmail.com>
2021-05-11 10:52:13 -0400

    updated Join-IdentityNowAccount to handle arrays

--- scripts/Join-IdentityNowAccount.ps1
+++ scripts/Join-IdentityNowAccount.ps1
@@ -7,6 +7,7 @@ function Join-IdentityNowAccount {
     Manually correlate an IdentityNow User Account with an identity account.
 
 .PARAMETER source
+    provide the source ID containing the accounts we wish to join
     SailPoint IdentityNow Source ID
     e.g 12345
 
@@ -16,47 +17,79 @@ function Join-IdentityNowAccount {
 .PARAMETER Account
     Account ID
 
+.PARAMETER org
+Specifies the identitynow org
+
+.PARAMETER joins
+provide a powershell object or array of objects with the property 'identity' and 'account'
+
 .EXAMPLE
     Join-IdentityNowAccount -source 12345 -identity jsmith -account 012345
 
+.EXAMPLE
+    $joins=@()
+    $joins+=[pscustomobject]@{
+            account = $account.nativeIdentity
+            displayName = $account.nativeIdentity
+            userName = $identity.name
+            type = $null
+        }
+    $joins | join-IdentityNowAccount -org $org -source $source.id
+    
 .LINK
     http://darrenjrobinson.com/sailpoint-identitynow
 
 #>
 
-    [cmdletbinding()]
+    [cmdletbinding(DefaultParameterSetName = 'SingleAccount')]
     param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [string]$account,
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ParameterSetName = 'SingleAccount')]    
+        [Parameter(Mandatory = $false, ParameterSetName = 'MultipleAccounts')]
+        [string]$org,    
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'SingleAccount')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'MultipleAccounts')]
         [string]$source,
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [string]$Identity
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'SingleAccount')]
+        [string]$account,
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'SingleAccount')]
+        [string]$Identity,
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'MultipleAccounts')]
+        [pscustomobject[]]$joins
     )
-
-    $v3Token = Get-IdentityNowAuth
-    
-    if ($v3Token.access_token) {
-        try {
-            $csv = @()
-            $csv = $csv + 'account,displayName,userName,type'
+    begin{
+        if ($org){set-identitynoworg $org}
+        $csv = @()
+        $csv = $csv + 'account,displayName,userName,type'
+        
+    }
+    process{
+        if ($account){
             $csv = $csv + "$account,$account,$identity,"
-            $result = Invoke-restmethod -Uri "https://$($IdentityNowConfiguration.orgName).api.identitynow.com/cc/api/source/loadUncorrelatedAccounts/$source" `
-                -Method "POST" `
-                -Headers @{Authorization = "$($v3Token.token_type) $($v3Token.access_token)"; "Accept-Encoding" = "gzip, deflate, br"} `
-                -ContentType "multipart/form-data; boundary=----WebKitFormBoundaryU1hSZTy7cff3WW27" `
-                -Body ([System.Text.Encoding]::UTF8.GetBytes("------WebKitFormBoundaryU1hSZTy7cff3WW27$([char]13)$([char]10)Content-Disposition: form-data; name=`"file`"; filename=`"temp.csv`"$([char]13)$([char]10)Content-Type: application/vnd.ms-excel$([char]13)$([char]10)$([char]13)$([char]10)$($csv | out-string)$([char]13)$([char]10)------WebKitFormBoundaryU1hSZTy7cff3WW27--$([char]13)$([char]10)")) `
-                -UseBasicParsing
-            return $result           
-        }
-        catch {
-            Write-Error "Account couldn't be joined. $($_)" 
+        }elseif($_){
+            $csv = $csv + "$_.account,$_.displayName,$_.identity,$_.type"
         }
     }
-    else {
-        Write-Error "Authentication Failed. Check your AdminCredential and v3 API ClientID and ClientSecret. $($_)"
-        return $v3Token
-    } 
+    end{
+        $v3Token = Get-IdentityNowAuth
+        if ($v3Token.access_token) {
+            try {
+                $result = Invoke-restmethod -Uri "https://$($IdentityNowConfiguration.orgName).api.identitynow.com/cc/api/source/loadUncorrelatedAccounts/$source" `
+                    -Method "POST" `
+                    -Headers @{Authorization = "$($v3Token.token_type) $($v3Token.access_token)"; "Accept-Encoding" = "gzip, deflate, br"} `
+                    -ContentType "multipart/form-data; boundary=----WebKitFormBoundaryU1hSZTy7cff3WW27" `
+                    -Body ([System.Text.Encoding]::UTF8.GetBytes("------WebKitFormBoundaryU1hSZTy7cff3WW27$([char]13)$([char]10)Content-Disposition: form-data; name=`"file`"; filename=`"temp.csv`"$([char]13)$([char]10)Content-Type: application/vnd.ms-excel$([char]13)$([char]10)$([char]13)$([char]10)$($csv | out-string)$([char]13)$([char]10)------WebKitFormBoundaryU1hSZTy7cff3WW27--$([char]13)$([char]10)")) `
+                    -UseBasicParsing
+                return $result           
+            }
+            catch {
+                Write-Error "Account couldn't be joined. $($_)" 
+            }
+        }
+        else {
+            Write-Error "Authentication Failed. Check your AdminCredential and v3 API ClientID and ClientSecret. $($_)"
+            return $v3Token
+        } 
+    }
 }
 
 

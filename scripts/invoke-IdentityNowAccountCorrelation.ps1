816163bda30e14e4694c8d93d15b3f2087210af3
Darren (Doc) Robinson <darren@darrenjrobinson.com>
2021-06-14 12:47:58 +1000

    Update invoke-IdentityNowAccountCorrelation.ps1
    Removed imbedded Join-IdentityNowAccount Function so the cmdlet from the module is used.

--- scripts/invoke-IdentityNowAccountCorrelation.ps1
+++ scripts/invoke-IdentityNowAccountCorrelation.ps1
@@ -1,34 +1,34 @@
 function Invoke-IdentityNowAccountCorrelation {
-<#
+    <#
     .SYNOPSIS
-        find uncorrelated accounts that can be joined
+        Find uncorrelated accounts that can be joined
 
     .DESCRIPTION
-        compare identities to a source's uncorrelated accounts to see if there are unjoined which would benefit from an unoptimized aggregation or manual correlation csv upload
+        Compare identities to a source's uncorrelated accounts to see if there are un-joined accounts which would benefit from an unoptimized aggregation or manual correlation csv upload
 
     .PARAMETER org
-    string, optional, the name of your org if you wish to switch, calls set-identitynoworg
+        string, optional, the name of your org if you wish to switch, calls Set-IdentityNowOrg
 
     .PARAMETER sourceName
-    string, required, the name of the source like "Corporate Active Directory", "ServiceNow", "AAD"
+        string, required, the name of the source like "Corporate Active Directory", "ServiceNow", "AAD"
 
     .PARAMETER identityAttribute
-    string, required, the system name of the identity attribute which will be tested for a match against accountAttribute
+        string, required, the system name of the identity attribute which will be tested for a match against accountAttribute
 
     .PARAMETER accountAttribute
-    string, required, the account attribute that should equal the value of identityAttribute, it could be userprincipalname, employeeid, or any other unique value
+        string, required, the account attribute that should equal the value of identityAttribute, it could be userprincipalname, employeeid, or any other unique value
 
     .PARAMETER missingAccountQuery
-    string, optional, the search query used to identify identities that are missing an account
-    the default will be "NOT @accounts(source.name:`"$sourcename`")"
-    in large environments, providing stricter criteria like, we also expect an account in AAD, or certain attributes should have a value, or only for this identity profile, can speed up the search query
-    IDN has a limit of 10,000 on their search, you may need to break up the identity results if necessary.
+        string, optional, the search query used to identify identities that are missing an account
+        the default will be "NOT @accounts(source.name:`"$sourcename`")"
+        in large environments, providing stricter criteria like, we also expect an account in AAD, or certain attributes should have a value, or only for this identity profile, can speed up the search query
+        IDN has a limit of 10,000 on their search, you may need to break up the identity results if necessary.
 
     .PARAMETER limit
-    integer, batch size for fetching identities and accounts for IDN API, default is 250
+        integer, batch size for fetching identities and accounts for IDN API, default is 250
     
     .PARAMETER triggerJoin
-    switch, after outputting joins will upload csv to IDN to manually correlate identities to accounts
+        switch, after outputting joins will upload csv to IDN to manually correlate identities to accounts
 
     .EXAMPLE
         Invoke-IdentityNowAccountCorrelation -sourceName "Prod AAD" -identityAttribute calculatedImmuteableID -accountAttribute immuteableId
@@ -50,183 +50,109 @@ function Invoke-IdentityNowAccountCorrelation {
         [string]$identityAttribute,
         [Parameter(Mandatory = $true)]
         [string]$accountAttribute,
-        [string]$missingAccountQuery="NOT @accounts(source.name:`"$sourcename`") AND attributes.$($identityAttribute):*",
+        [string]$missingAccountQuery = "NOT @accounts(source.name:`"$sourcename`") AND attributes.$($identityAttribute):*",
         [ValidateRange(0, 250)]
-        [int]$limit=250,
+        [int]$limit = 250,
         [switch]$triggerJoin
     )
-    if ($org){set-identitynoworg $org}
-    try{
-        $org=(get-identitynoworg).'Organisation Name'
-    }catch{
-        throw "possibly missing sailpointidentitynow module:$_"
+    if ($org) { 
+        Set-IdentityNowOrg $org 
+    }
+
+    try {
+        $org = (Get-IdentityNowOrg).'Organisation Name'
+    }
+    catch {
+        throw "Possibly missing SailpointIdentityNow PowerShell module: $_"
     }
     
-    $searchBody=[pscustomobject]@{
+    $searchBody = [pscustomobject]@{
         indices = @("identities")
-        query = [pscustomobject]@{
-            query = $missingAccountQuery
-            fields = @("name","description")
+        query   = [pscustomobject]@{
+            query  = $missingAccountQuery
+            fields = @("name", "description")
         }
     }
-    $source=Get-IdentityNowSource
-    $source=$source.where{$_.name -eq $sourcename}[0]
-    $auth=Get-IdentityNowAuth
-    $i=0
-    $accounts=@()
-    write-output "getting from beta accounts API 'sourceId eq `"$($source.externalId)`" and uncorrelated eq true'"
-    do{
-        $url="https://$org.api.identitynow.com/beta/accounts?count=true&limit=$limit&offset=$($limit*$i)&filters=sourceId eq `"$($source.externalId)`" and uncorrelated eq true"
-        try{
-            $temp=Invoke-RestMethod -UseBasicParsing -Uri $url -Headers @{"Authorization"="Bearer $($auth.access_token)"} -Method Get
-        }catch{
-            switch($_.Exception.Response.StatusCode){
-                'GatewayTimeout'{Write-Error "$($_.Exception.Response.StatusCode):$_"}
-                default{"$($_.Exception.Response.StatusCode):$_"}
+    $source = Get-IdentityNowSource
+    $source = $source.where{ $_.name -eq $sourcename }[0]
+    $auth = Get-IdentityNowAuth -return V3JWT
+    $i = 0
+    $accounts = @()
+    write-output "Getting from beta accounts API 'sourceId eq `"$($source.externalId)`" and uncorrelated eq true'"
+
+    do {
+        $url = "https://$org.api.identitynow.com/beta/accounts?count=true&limit=$limit&offset=$($limit*$i)&filters=sourceId eq `"$($source.externalId)`" and uncorrelated eq true"
+        try {
+            $temp = Invoke-RestMethod -UseBasicParsing `
+                -Uri $url `
+                -Headers @{"Authorization" = "Bearer $($auth.access_token)" } `
+                -Method Get
+        }
+        catch {
+            switch ($_.Exception.Response.StatusCode) {
+                'GatewayTimeout' { Write-Error "$($_.Exception.Response.StatusCode):$_" }
+                default { "$($_.Exception.Response.StatusCode):$_" }
             }
         }
-        if ($temp.count -eq 1){$temp=ConvertFrom-Json ($temp -creplace '\"ImmutableId\"\:(null|\"[\w\d\\\+\-\@\.\/]{1,}\"),','')}
-        $accounts+=$temp
+
+        if ($temp.count -eq 1) { 
+            $temp = ConvertFrom-Json ($temp -creplace '\"ImmutableId\"\:(null|\"[\w\d\\\+\-\@\.\/]{1,}\"),', '') 
+        }
+
+        $accounts += $temp
         $i++
         write-progress -activity 'get accounts' -status $accounts.Count
-    }until($temp.count -lt $limit)
+    } until ($temp.count -lt $limit)
+
     write-output "retrieved $($accounts.count)"
-    $auth=Get-IdentityNowAuth
-    $i=0
-    $missingaccount=@()
-    write-output "getting from identities from v3 search API:$missingAccountQuery"
-    do{
-        $url="https://$org.api.identitynow.com/v3/search?count=true&limit=$limit&offset=$($limit*$i)"
-        $temp=$null
-        $temp=Invoke-RestMethod -UseBasicParsing -Uri $url -Headers @{"Authorization"="Bearer $($auth.access_token)"} -Method Post -Body ($searchBody | ConvertTo-Json) -ContentType 'application/json'
-        if ($temp.count -ge 1){$missingaccount+=$temp}
-        if ($temp.Count -eq $limit){$i++}
+    $auth = Get-IdentityNowAuth -return V3JWT
+    $i = 0
+    $missingaccount = @()
+    write-output "getting Identities from v3 search API:$missingAccountQuery"
+    do {
+        $url = "https://$org.api.identitynow.com/v3/search?count=true&limit=$limit&offset=$($limit*$i)"
+        $temp = $null
+        $temp = Invoke-RestMethod -UseBasicParsing `
+            -Uri $url `
+            -Headers @{"Authorization" = "Bearer $($auth.access_token)" } `
+            -Method Post `
+            -Body ($searchBody | ConvertTo-Json) `
+            -ContentType 'application/json'
+        
+        if ($temp.count -ge 1) { 
+            $missingaccount += $temp 
+        }
+        if ($temp.Count -eq $limit) { 
+            $i++ 
+        }
         write-progress -activity 'get identities' -status $missingaccount.Count
-    }until($temp.count -lt $limit)
+    } until ($temp.count -lt $limit)
+
     write-output "retrieved $($missingAccount.count) identities"
-    $i=0
-    $joins=@()
-    foreach($user in $missingaccount){
+    $i = 0
+    $joins = @()
+    foreach ($user in $missingaccount) {
         $i++
-        if ($user.attributes.$identityAttribute -in $accounts.attributes.$accountAttribute){
-            $joins+=[pscustomobject]@{
-                account = $accounts.where{$_.attributes.$accountAttribute -eq $user.attributes.$identityAttribute}.nativeIdentity
-                displayName = $accounts.where{$_.attributes.$accountAttribute -eq $user.attributes.$identityAttribute}.nativeIdentity
-                userName = $user.name
-                type = $null
+        if ($user.attributes.$identityAttribute -in $accounts.attributes.$accountAttribute) {
+            $joins += [pscustomobject]@{
+                account     = $accounts.where{ $_.attributes.$accountAttribute -eq $user.attributes.$identityAttribute }.nativeIdentity
+                displayName = $accounts.where{ $_.attributes.$accountAttribute -eq $user.attributes.$identityAttribute }.nativeIdentity
+                userName    = $user.name
+                type        = $null
             }
             write-output $joins[-1] | ConvertTo-Json
         }
     }
-function Join-IdentityNowAccount {
-    <#
-        .SYNOPSIS
-            Join an IdentityNow User Account to an Identity.
 
-        .DESCRIPTION
-            Manually correlate an IdentityNow User Account with an identity account.
-
-        .PARAMETER source
-            provide the source ID containing the accounts we wish to join
-            SailPoint IdentityNow Source ID
-            e.g 12345
-
-        .PARAMETER Identity
-            Identity UID
-
-        .PARAMETER Account
-            Account ID
-
-        .PARAMETER org
-        Specifies the identitynow org
-
-        .PARAMETER joins
-        provide a powershell object or array of objects with the property 'identity' and 'account'
-
-        .EXAMPLE
-            Join-IdentityNowAccount -source 12345 -identity jsmith -account 012345
-
-        .EXAMPLE
-            $joins=@()
-            $joins+=[pscustomobject]@{
-                    account = $account.nativeIdentity
-                    displayName = $account.nativeIdentity
-                    userName = $identity.name
-                    type = $null
-                }
-            $joins | join-IdentityNowAccount -org $org -source $source.id
-            
-        .LINK
-            http://darrenjrobinson.com/sailpoint-identitynow
-
-    #>
-
-    [cmdletbinding(DefaultParameterSetName = 'SingleAccount')]
-    param(
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ParameterSetName = 'SingleAccount')]    
-        [Parameter(Mandatory = $false, ParameterSetName = 'MultipleAccounts')]
-        [string]$org,    
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'SingleAccount')]
-        [Parameter(Mandatory = $true, ParameterSetName = 'MultipleAccounts')]
-        [string]$source,
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'SingleAccount')]
-        [string]$account,
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'SingleAccount')]
-        [string]$Identity,
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'MultipleAccounts')]
-        [pscustomobject[]]$joins
-    )
-    begin{
-        if ($org){set-identitynoworg $org}
-        try{
-            $org=(get-identitynoworg).'Organisation Name'
-        }catch{
-            throw "possibly missing sailpointidentitynow module:$_"
-        }
-        $csv = @()
-        $csv = $csv + 'account,displayName,userName,type'
-        
-    }
-    process{
-        if ($account){
-            $csv = $csv + "$account,$account,$identity,"
-        }elseif($_){
-            $csv = $csv + "$($_.account),$($_.displayName),$($_.userName),$($_.type)"
-        }
-    }
-    end{
-        $v3Token = Get-IdentityNowAuth
-        if ($v3Token.access_token) {
-            try {
-                $result = Invoke-restmethod -Uri "https://$org.api.identitynow.com/cc/api/source/loadUncorrelatedAccounts/$source" `
-                    -Method "POST" `
-                    -Headers @{Authorization = "$($v3Token.token_type) $($v3Token.access_token)"; "Accept-Encoding" = "gzip, deflate, br"} `
-                    -ContentType "multipart/form-data; boundary=----WebKitFormBoundaryU1hSZTy7cff3WW27" `
-                    -Body ([System.Text.Encoding]::UTF8.GetBytes("------WebKitFormBoundaryU1hSZTy7cff3WW27$([char]13)$([char]10)Content-Disposition: form-data; name=`"file`"; filename=`"temp.csv`"$([char]13)$([char]10)Content-Type: application/vnd.ms-excel$([char]13)$([char]10)$([char]13)$([char]10)$($csv | out-string)$([char]13)$([char]10)------WebKitFormBoundaryU1hSZTy7cff3WW27--$([char]13)$([char]10)")) `
-                    -UseBasicParsing
-                return $result           
-            }
-            catch {
-                Write-Error "Account couldn't be joined. $($_)" 
-            }
-        }
-        else {
-            Write-Error "Authentication Failed. Check your AdminCredential and v3 API ClientID and ClientSecret. $($_)"
-            return $v3Token
-        } 
-    }
-}
-
-    if ($triggerJoin -and $joins.count -ge 1){
+    if ($triggerJoin -and $joins.count -ge 1) {
         $joins | Join-IdentityNowAccount -org $org -source $source.id
     }
-
 }
 # SIG # Begin signature block
 # MIINSwYJKoZIhvcNAQcCoIINPDCCDTgCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUXLlCE3muqplNfMZ6/hqDePN5
-# hhigggqNMIIFMDCCBBigAwIBAgIQBAkYG1/Vu2Z1U0O1b5VQCDANBgkqhkiG9w0B
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUy17btNg888z9+ePtOkFhk4FX
+# NdegggqNMIIFMDCCBBigAwIBAgIQBAkYG1/Vu2Z1U0O1b5VQCDANBgkqhkiG9w0B
 # AQsFADBlMQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYD
 # VQQLExB3d3cuZGlnaWNlcnQuY29tMSQwIgYDVQQDExtEaWdpQ2VydCBBc3N1cmVk
 # IElEIFJvb3QgQ0EwHhcNMTMxMDIyMTIwMDAwWhcNMjgxMDIyMTIwMDAwWjByMQsw
@@ -287,11 +213,11 @@ function Join-IdentityNowAccount {
 # b20xMTAvBgNVBAMTKERpZ2lDZXJ0IFNIQTIgQXNzdXJlZCBJRCBDb2RlIFNpZ25p
 # bmcgQ0ECEAzs0XV3s4G5ExftUKPGYK8wCQYFKw4DAhoFAKB4MBgGCisGAQQBgjcC
 # AQwxCjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYKKwYB
-# BAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFFZKwgcbJj/x
-# AAq7XQMBqTkUtSR1MA0GCSqGSIb3DQEBAQUABIIBAAMaDRDo/j9OK5krjtDh481d
-# oJAo5oVcXSk82gXltoLAY/nZH3DIiehgmC55i40E0FOSoJXi0NC++25vWwXzTfl7
-# mWU20iYzHGRfzJcBb5h1/pKxSiOYByqHDUtQZMnZo1MRyXeHgMmuO/yyol/MgMQ5
-# xBoSiR5ux3RKh7YAEVx9ckwDg4jWgwYJnwr07gYTxRb4x5VOTh195bzhmgRdBWCt
-# rFJpsKW7IvXxXcxMvVatWgU1fvJ1kCOsi2y44vldMwaNnoC8j0hx/OD7HB0C73mm
-# dViUGFxB7/eFhnjv4E6VP+neO8uv9xbEqBEbahNlNJj/Arj/2YPzEcM5hVS3FkM=
+# BAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFMMEGHjD92r3
+# OByEed00RXIb61XgMA0GCSqGSIb3DQEBAQUABIIBAF3iQIOPTIv+hN9zL5X8cVSl
+# 303sawO416f/u3Qf870GbvHUJNJ9dPkv5FWHZHYaSi2dIZUoNFh71VfHV+0Dj0hZ
+# qQGqt1cs25Ix0+fJItoAgDmLOMG7s7Zoklo+iowRqRBtODZEgB0sayzC+6fjTeCz
+# jfRYQEYmYkgJ1FUfmYjB0OEKdIGxl0tmc8a6Mew4sUx6NrmIGTCzBITdDgb+QqyM
+# 1DIFwTcu9mDhNuF+4jJnD6Hnito+noJSiUGLdW8SxCdWHWRUDoRcK/+nQtApH57D
+# FU8RA9+cEjJwRT61q+JWEw2Ulo0+Ns8Vva4fgEUJ0bAqtCqWbs3zjzAcfaGEcrM=
 # SIG # End signature block
